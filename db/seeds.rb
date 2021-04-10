e6d945c15a328f5924a15f2fe91b4f31405fd55f
kamkara <lnclass.kamkara@gmail.com>
2021-04-10 11:48:46 +0000

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+500.times do
+    name = Faker::Company.name 
+    content = Faker::Lorem.sentence 
+    quantity = rand(0..100)
+    Post.create(name:name, content:content, quantity:quantity)
+end
